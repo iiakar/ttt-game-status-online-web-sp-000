88c9450ffc63cca6966e57148164452c788a3a4d
Ibrahim  Akar <ibsakar@gmail.com>
2019-09-28 13:00:48 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,7 +17,13 @@ WIN_COMBINATIONS = [
   [2,4,6]
   ]
   
-  
+  def won?(board)
+  WIN_COMBINATIONS.detect do |combo|
+    board[combo[0]] == board[combo[1]] &&
+    board[combo[1]] == board[combo[2]] &&
+    position_taken?(board, combo[0])
+  end
+end
 
 #   def won?(board)
 #   WIN_COMBINATIONS.each  {|win_combo|
